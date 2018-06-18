9c961210272707c81fb9254fd7ecf001d365c1b4
melody2m <melody2musik@gmail.com>
2018-06-18 14:34:17 -0700

    basic socket server

---
+++ src/lib/io-commands.js
@@ -0,0 +1,16 @@
+
+export default (ioServer) => {
+    ioServer.on('connection', socket => {
+        console.log('__CONNECTION__', socket.id);
+
+        socket.on(SEND_MESSAGE, (data) => {
+            console.log('__SOCKET_EVENT__', SEND_MESSAGE);
+            socket.emit(RECEIVE_MESSAGE, 'You have sent a message.')
+            ioServer.emit(RECEIVE_MESSAGE, {
+              ...data,
+              id: uuid(),
+              timestamp: new Date(),
+            });
+          });
+        });
+    }
