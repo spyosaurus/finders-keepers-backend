91108a494f5c499b5720babf5cfdf1533e4b9ee9
Jennifer Williams <jennifer@squareandplum.com>
2018-06-20 13:03:11 -0700

    send plain text message to connected sockets, rather than JSON

--- src/lib/io-commands.js
+++ src/lib/io-commands.js
@@ -7,21 +7,14 @@ const players = [];
 export default (ioServer) => {
   ioServer.on('connection', (socket) => {
     console.log('__CONNECTION__', socket.id);
-    players.push(socket.id);
 
     socket.on(SEND_MESSAGE, (data) => {
       console.log('__SOCKET_EVENT__', SEND_MESSAGE);
-      socket.emit(RECEIVE_MESSAGE, 'You have sent a message.');
-      ioServer.sockets.emit(RECEIVE_MESSAGE, {
-        ...data,
-        id: uuid(),
-        timestamp: new Date(),
-      });
+
+      ioServer.sockets.emit(RECEIVE_MESSAGE, data);
     });
   });
   ioServer.on('disconnect', (socket) => {
     console.log('__DISCONNECTION__', socket.id);
-
   });
-
 };
