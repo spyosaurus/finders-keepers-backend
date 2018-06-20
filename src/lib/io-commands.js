3aa9fe347799bb3aa06b8a5cce2adcfc4e848009
Jennifer Williams <jennifer@squareandplum.com>
2018-06-20 10:16:34 -0700

    socket emit test

--- src/lib/io-commands.js
+++ src/lib/io-commands.js
@@ -2,19 +2,26 @@ import uuid from 'uuid';
 
 const SEND_MESSAGE = 'SEND_MESSAGE';
 const RECEIVE_MESSAGE = 'RECEIVE_MESSAGE';
+const players = [];
 
 export default (ioServer) => {
   ioServer.on('connection', (socket) => {
     console.log('__CONNECTION__', socket.id);
+    players.push(socket.id);
 
     socket.on(SEND_MESSAGE, (data) => {
       console.log('__SOCKET_EVENT__', SEND_MESSAGE);
       socket.emit(RECEIVE_MESSAGE, 'You have sent a message.');
-      ioServer.emit(RECEIVE_MESSAGE, {
+      ioServer.sockets.emit(RECEIVE_MESSAGE, {
         ...data,
         id: uuid(),
         timestamp: new Date(),
       });
     });
   });
+  ioServer.on('disconnect', (socket) => {
+    console.log('__DISCONNECTION__', socket.id);
+
+  });
+
 };
