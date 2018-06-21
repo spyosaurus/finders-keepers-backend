e1ec3d5e79435c7517d03c23a7a3b6ff27495a22
cara ottmar <cara.ottmar@gmail.com>
2018-06-21 11:39:37 -0700

    changed package.json for deployment.

---
+++ build/lib/server.js
@@ -0,0 +1,78 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.stopServer = exports.startServer = undefined;
+
+var _cors = require('cors');
+
+var _cors2 = _interopRequireDefault(_cors);
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _mongoose = require('mongoose');
+
+var _mongoose2 = _interopRequireDefault(_mongoose);
+
+var _logger = require('./logger');
+
+var _logger2 = _interopRequireDefault(_logger);
+
+var _errorMiddleware = require('./error-middleware');
+
+var _errorMiddleware2 = _interopRequireDefault(_errorMiddleware);
+
+var _accountRouter = require('../route/account-router');
+
+var _accountRouter2 = _interopRequireDefault(_accountRouter);
+
+var _ioCommands = require('./io-commands');
+
+var _ioCommands2 = _interopRequireDefault(_ioCommands);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+require('dotenv').config();
+
+var app = (0, _express2.default)();
+var server = null;
+
+app.use((0, _cors2.default)({
+  origin: 'http://localhost:8080',
+  credentials: true
+}));
+app.use(_accountRouter2.default);
+
+app.all('*', function (request, response) {
+  _logger2.default.log(_logger2.default.INFO, 'Returning a 404 from the catch-all/default route');
+  return response.sendStatus(404);
+});
+
+app.use(_errorMiddleware2.default);
+
+var startServer = function startServer() {
+  return _mongoose2.default.connect(process.env.MONGODB_URI).then(function () {
+    server = app.listen(process.env.PORT, function () {
+      _logger2.default.log(_logger2.default.INFO, 'Server is listening on port ' + process.env.PORT);
+      var ioServer = require('socket.io')(server);
+
+      ioServer.rooms = {};
+
+      (0, _ioCommands2.default)(ioServer);
+    });
+  });
+};
+
+var stopServer = function stopServer() {
+  return _mongoose2.default.disconnect().then(function () {
+    server.close(function () {
+      _logger2.default.log(_logger2.default.INFO, 'Server is off');
+    });
+  });
+};
+
+exports.startServer = startServer;
+exports.stopServer = stopServer;
